7b260b67992429a187229ba9ae160784d32e0180
Diego <dpardoblacutt@gmail.com>
2023-03-13 17:58:31 -0400

    Add initial structure of test_order

---
+++ app/test/services/test_order.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_create_order_service():
+    pytest.assume(False)
+
+def test_update_order_service():
+    pytest.assume(False)
+
+def test_get_order_by_id_service():
+    pytest.assume(False)
+
+def test_get_orders_service():
+    pytest.assume(False)
