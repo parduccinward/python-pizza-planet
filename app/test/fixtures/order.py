7420c6553cb0fadfc54390f2d856799d9d524bdc
Diego <dpardoblacutt@gmail.com>
2023-03-13 20:10:02 -0400

    Make test for the service of create an order

--- app/test/fixtures/order.py
+++ app/test/fixtures/order.py
@@ -15,7 +15,7 @@ def client_data_mock() -> dict:
 
 @pytest.fixture
 def order_uri():
-    return '/order'
+    return '/order/'
 
 
 @pytest.fixture
@@ -34,16 +34,27 @@ def order(create_ingredients, create_size, client_data) -> dict:
     }
 
 
-@pytest.fixture
-def create_orders(client, order_uri, create_ingredients, create_sizes) -> list:
+def create_order(client, order_uri, create_ingredients, create_sizes):
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
     sizes = [size.get('_id') for size in create_sizes]
+    order = client.post(order_uri, json={
+        **client_data_mock(),
+        'ingredients': shuffle_list(ingredients)[:5],
+        'size_id': shuffle_list(sizes)[0]
+    })
+    return order
+
+
+@pytest.fixture
+def create_order_dict(client, order_uri, create_ingredients, create_sizes) -> dict:
+    return create_order(client, order_uri, create_ingredients, create_sizes)
+
+
+@pytest.fixture
+def create_orders_list(client, order_uri, create_ingredients, create_sizes) -> list:
     orders = []
     for _ in range(10):
-        new_order = client.post(order_uri, json={
-            **client_data_mock(),
-            'ingredients': shuffle_list(ingredients)[:5],
-            'size_id': shuffle_list(sizes)[0]
-        })
+        new_order = create_order(
+            client, order_uri, create_ingredients, create_sizes)
         orders.append(new_order)
     return orders
--- app/test/services/test_order.py
+++ app/test/services/test_order.py
@@ -1,13 +1,29 @@
 import pytest
 
-def test_create_order_service():
-    pytest.assume(False)
 
+def test_create_order_service(create_order_dict):
+    order = create_order_dict.json
+    pytest.assume(create_order_dict.status.startswith('200'))
+    pytest.assume(order['_id'])
+    pytest.assume(order['client_address'])
+    pytest.assume(order['client_dni'])
+    pytest.assume(order['client_name'])
+    pytest.assume(order['client_phone'])
+    pytest.assume(order['date'])
+    pytest.assume(order['detail'])
+    pytest.assume(order['size'])
+
+
+@pytest.mark.skip(reason="test not implemented yet")
 def test_update_order_service():
     pytest.assume(False)
 
+
+@pytest.mark.skip(reason="test not implemented yet")
 def test_get_order_by_id_service():
     pytest.assume(False)
 
+
+@pytest.mark.skip(reason="test not implemented yet")
 def test_get_orders_service():
     pytest.assume(False)
